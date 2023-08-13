c9b4b2c3f5655cbec9850d3baef95e8de4595168
martinezlaurinha <142118089+martinezlaurinha@users.noreply.github.com>
2023-08-13 19:53:22 -0300

    adicionando arquivos js e css

---
+++ sktech.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(600, 600);background("green");
+    }
+    function draw() {
+    rect(mouseX, mouseY, 130);fill("yellow")
+      circle(300, 400, 200);fill("blue")
+      rect(30, 20, 200, 300);
+      fill("violet");
+      if(mouseIsPressed);
+      fill("red")
+    }
